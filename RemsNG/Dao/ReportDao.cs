dc5cfe3d0b43d1614fd0a96dd4fc44d08fb96a6c
Aguda Olabode H <olabodeaguda@outlook.com>
2018-01-04 14:38:42 +0100

    update

---
+++ RemsNG/Dao/ReportDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RemsNG.ORM;
+
+namespace RemsNG.Dao
+{
+    public class ReportDao : AbstractDao
+    {
+        public ReportDao(RemsDbContext _db) : base(_db)
+        {
+        }
+
+
+
+
+    }
+}
