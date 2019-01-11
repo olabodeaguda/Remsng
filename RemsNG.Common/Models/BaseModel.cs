0f807b3133d3dfa464834cba8e5bb0e9c0e481c6
Aguda Olabode H <olabodeaguda@outlook.com>
2019-01-11 13:11:34 +0100

    UI update

---
+++ RemsNG.Common/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemsNG.Common.Models
+{
+    public class BaseModel
+    {
+        public string createdBy { get; set; }
+        public DateTimeOffset dateCreated { get; set; } = DateTimeOffset.Now;
+        public string lastmodifiedby { get; set; }
+        public DateTime? lastModifiedDate { get; set; }
+    }
+}
